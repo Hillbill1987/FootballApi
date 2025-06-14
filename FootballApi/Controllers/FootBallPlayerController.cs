b1ab370d3d0f3bc0a441a3a1c213d853eb41ad9f
Graeme Hill <49688915+Hillbill1987@users.noreply.github.com>
2025-06-14 15:16:29 +0100

    implementation of new controllers and dummy data tested (#2)

---
+++ FootballApi/Controllers/FootBallPlayerController.cs
@@ -0,0 +1,92 @@
+﻿using FootballApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi.Validations;
+
+namespace FootballApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FootBallPlayerController : ControllerBase
+    {
+        static private List<FootballPlayer> players = new List<FootballPlayer>
+        {
+            new FootballPlayer
+            {
+                Id = 1,
+                Name = "Dave Rogers",
+                Team = "Rushie United",
+                Nationality = "Scottish",
+                Age = 23
+            },
+            new FootballPlayer
+            {
+                Id = 2,
+                Name = "Jimmy Fred",
+                Team = "Astro Rovers",
+                Nationality = "French",
+                Age = 30
+            },
+            new FootballPlayer
+            {
+                Id = 3,
+                Name = "Roy Kollow",
+                Team = "Red Warriors",
+                Nationality = "Danish",
+                Age = 27
+            }
+        };
+
+        [HttpGet]
+        public ActionResult<List<FootballPlayer>> Getplayers()
+        {
+            return Ok(players);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<FootballPlayer> GetPlayerById(int id)
+        {
+            var player = players.FirstOrDefault(x => x.Id == id);
+            if (player == null)
+                return NotFound();
+            return Ok(player);
+        }
+
+        [HttpPost]
+        public ActionResult AddPlayer(FootballPlayer newPlayer)
+        {
+            if (newPlayer == null)
+                return BadRequest();
+            players.Add(newPlayer);
+            return CreatedAtAction(nameof(GetPlayerById), new { id = newPlayer.Id }, newPlayer);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdatePlayer(int id, FootballPlayer updatedPlayer)
+        {
+           var player = players.FirstOrDefault(x => x.Id == id);
+           if(player == null)
+                return NotFound();
+
+           player.Id = updatedPlayer.Id;
+           player.Name = updatedPlayer.Name;
+           player.Age = updatedPlayer.Age;
+           player.Team = updatedPlayer.Team;
+           player.Nationality = updatedPlayer.Nationality;
+
+            return Created();
+
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeletePlayer(int id)
+        {
+            var player = players.FirstOrDefault(x => x.Id == id);
+            if (player == null)
+                return NotFound();
+
+            players.Remove(player);
+            return Ok();
+        }
+    }
+}
---
+++ FootballApi/Controllers/FootballTeamController.cs
@@ -0,0 +1,87 @@
+﻿using FootballApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FootballApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FootballTeamController : ControllerBase
+    {
+        static private List<FootballTeam> teams = new List<FootballTeam>
+        {
+            new FootballTeam
+            {
+                Id = 1,
+                Name = "Yuko warriors",
+                YearFounded = 1945,
+                Country = "France",
+                Manager = "Mr T"
+            },
+            new FootballTeam
+            {
+                Id = 2,
+                Name = "Green Lamp United",
+                YearFounded = 1890,
+                Country = "England",
+                Manager = "Mr Q"
+            },
+            new FootballTeam
+            {
+                Id = 3,
+                Name = "Red Sea United",
+                YearFounded = 1956,
+                Country = "Brazil",
+                Manager = "Mr Z"
+            }
+        };
+
+        [HttpGet]
+        public ActionResult<List<FootballTeam>> GetTeams()
+        {
+            return Ok(teams);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<FootballTeam> GetTeamByid(int id)
+        {
+            var team = teams.FirstOrDefault(t => t.Id == id);
+            if (team == null)
+                return NotFound();
+            return Ok(team);
+        }
+
+        [HttpPost]
+        public ActionResult<FootballTeam> AddTeam(FootballTeam newTeam)
+        {
+            if (newTeam == null)
+                return BadRequest();
+            teams.Add(newTeam);
+            return CreatedAtAction(nameof(GetTeamByid), new { id = newTeam.Id }, newTeam);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult updateTeam(int id, FootballTeam updatedTeam)
+        {
+            var team = teams.FirstOrDefault(x => x.Id == id);
+            if (team == null)
+                return NotFound();
+            team.Name = updatedTeam.Name;
+            team.YearFounded = updatedTeam.YearFounded;
+            team.Manager = updatedTeam.Manager;
+            team.Country = updatedTeam.Country;
+
+            return Created();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteTeam(int id)
+        {
+            var team = teams.FirstOrDefault(x => x.Id == id);
+            if (team == null)
+                return NotFound();
+            teams.Remove(team);
+            return Ok();
+        }
+    }
+}
