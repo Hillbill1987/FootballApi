90957e5f5e56eae449ae4f943c49a8cb6a28bb68
Graeme Hill <49688915+Hillbill1987@users.noreply.github.com>
2025-06-17 16:26:48 +0100

    added db context and tested with sql

--- FootballApi/Controllers/FootballTeamController.cs
+++ FootballApi/Controllers/FootballTeamController.cs
@@ -1,6 +1,9 @@
-﻿using FootballApi.Models;
+﻿using FootballApi.Data;
+using FootballApi.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
 
 namespace FootballApi.Controllers
 {
@@ -8,62 +11,69 @@ namespace FootballApi.Controllers
     [ApiController]
     public class FootballTeamController : ControllerBase
     {
-        static private List<FootballTeam> teams = new List<FootballTeam>
+        //static private List<FootballTeam> teams = new List<FootballTeam>
+        //{
+        //    new FootballTeam
+        //    {
+        //        Id = 1,
+        //        Name = "Yuko warriors",
+        //        YearFounded = 1945,
+        //        Country = "France",
+        //        Manager = "Mr T"
+        //    },
+        //    new FootballTeam
+        //    {
+        //        Id = 2,
+        //        Name = "Green Lamp United",
+        //        YearFounded = 1890,
+        //        Country = "England",
+        //        Manager = "Mr Q"
+        //    },
+        //    new FootballTeam
+        //    {
+        //        Id = 3,
+        //        Name = "Red Sea United",
+        //        YearFounded = 1956,
+        //        Country = "Brazil",
+        //        Manager = "Mr Z"
+        //    }
+        //};
+
+        private readonly DataContext _dataContext;
+        public FootballTeamController(DataContext context)
         {
-            new FootballTeam
-            {
-                Id = 1,
-                Name = "Yuko warriors",
-                YearFounded = 1945,
-                Country = "France",
-                Manager = "Mr T"
-            },
-            new FootballTeam
-            {
-                Id = 2,
-                Name = "Green Lamp United",
-                YearFounded = 1890,
-                Country = "England",
-                Manager = "Mr Q"
-            },
-            new FootballTeam
-            {
-                Id = 3,
-                Name = "Red Sea United",
-                YearFounded = 1956,
-                Country = "Brazil",
-                Manager = "Mr Z"
-            }
-        };
+            _dataContext = context;
+        }
 
         [HttpGet]
-        public ActionResult<List<FootballTeam>> GetTeams()
+        public async Task<ActionResult<List<FootballTeam>>> GetTeams()
         {
-            return Ok(teams);
+            return Ok(await _dataContext.teams.ToListAsync());
         }
 
         [HttpGet("{id}")]
-        public ActionResult<FootballTeam> GetTeamByid(int id)
+        public async Task<ActionResult<FootballTeam>> GetTeamByid(int id)
         {
-            var team = teams.FirstOrDefault(t => t.Id == id);
+            var team = await _dataContext.teams.FindAsync(id);
             if (team == null)
                 return NotFound();
             return Ok(team);
         }
 
         [HttpPost]
-        public ActionResult<FootballTeam> AddTeam(FootballTeam newTeam)
+        public async Task<ActionResult<FootballTeam>> AddTeam(FootballTeam newTeam)
         {
             if (newTeam == null)
                 return BadRequest();
-            teams.Add(newTeam);
+            _dataContext.teams.Add(newTeam);
+            await _dataContext.SaveChangesAsync();
             return CreatedAtAction(nameof(GetTeamByid), new { id = newTeam.Id }, newTeam);
         }
 
         [HttpPut("{id}")]
-        public IActionResult updateTeam(int id, FootballTeam updatedTeam)
+        public async Task<IActionResult> updateTeam(int id, FootballTeam updatedTeam)
         {
-            var team = teams.FirstOrDefault(x => x.Id == id);
+            var team = await _dataContext.teams.FindAsync(id);
             if (team == null)
                 return NotFound();
             team.Name = updatedTeam.Name;
@@ -71,16 +81,19 @@ namespace FootballApi.Controllers
             team.Manager = updatedTeam.Manager;
             team.Country = updatedTeam.Country;
 
+            await _dataContext.SaveChangesAsync();
+
             return Created();
         }
 
         [HttpDelete("{id}")]
-        public IActionResult DeleteTeam(int id)
+        public async Task<IActionResult> DeleteTeam(int id)
         {
-            var team = teams.FirstOrDefault(x => x.Id == id);
+            var team = await _dataContext.teams.FindAsync(id);
             if (team == null)
                 return NotFound();
-            teams.Remove(team);
+            _dataContext.teams.Remove(team);
+            await _dataContext.SaveChangesAsync();
             return Ok();
         }
     }
---
+++ FootballApi/Migrations/20250617150454_added_data.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace FootballApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class added_data : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "players",
+                columns: new[] { "Id", "Age", "Name", "Nationality", "Team" },
+                values: new object[,]
+                {
+                    { 1, 23, "Dave Rogers", "Scottish", "Rushie United" },
+                    { 2, 30, "Jimmy Fred", "French", "Astro Rovers" },
+                    { 3, 27, "Roy Kollow", "Danish", "Red Warriors" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "teams",
+                columns: new[] { "Id", "Country", "Manager", "Name", "YearFounded" },
+                values: new object[,]
+                {
+                    { 1, "France", "Mr T", "Yuko warriors", 1945 },
+                    { 2, "England", "Mr Q", "Green Lamp United", 1890 },
+                    { 3, "Brazil", "Mr Z", "Red Sea United", 1956 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "players",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "players",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "players",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "teams",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "teams",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "teams",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
