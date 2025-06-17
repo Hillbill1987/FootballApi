4bd24e426e21fa030b85ed36c55b2767ca9a08fb
Graeme Hill <49688915+Hillbill1987@users.noreply.github.com>
2025-06-17 16:26:35 +0100

    added db context and tested with sql (#3)

--- FootballApi/Controllers/FootBallPlayerController.cs
+++ FootballApi/Controllers/FootBallPlayerController.cs
@@ -1,6 +1,8 @@
-﻿using FootballApi.Models;
+﻿using FootballApi.Data;
+using FootballApi.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Validations;
 
 namespace FootballApi.Controllers
@@ -9,83 +11,92 @@ namespace FootballApi.Controllers
     [ApiController]
     public class FootBallPlayerController : ControllerBase
     {
-        static private List<FootballPlayer> players = new List<FootballPlayer>
+        //static private List<FootballPlayer> players = new List<FootballPlayer>
+        //{
+        //    new FootballPlayer
+        //    {
+        //        Id = 1,
+        //        Name = "Dave Rogers",
+        //        Team = "Rushie United",
+        //        Nationality = "Scottish",
+        //        Age = 23
+        //    },
+        //    new FootballPlayer
+        //    {
+        //        Id = 2,
+        //        Name = "Jimmy Fred",
+        //        Team = "Astro Rovers",
+        //        Nationality = "French",
+        //        Age = 30
+        //    },
+        //    new FootballPlayer
+        //    {
+        //        Id = 3,
+        //        Name = "Roy Kollow",
+        //        Team = "Red Warriors",
+        //        Nationality = "Danish",
+        //        Age = 27
+        //    }
+        //};
+        private readonly DataContext _dataContext;
+        public FootBallPlayerController(DataContext context)
         {
-            new FootballPlayer
-            {
-                Id = 1,
-                Name = "Dave Rogers",
-                Team = "Rushie United",
-                Nationality = "Scottish",
-                Age = 23
-            },
-            new FootballPlayer
-            {
-                Id = 2,
-                Name = "Jimmy Fred",
-                Team = "Astro Rovers",
-                Nationality = "French",
-                Age = 30
-            },
-            new FootballPlayer
-            {
-                Id = 3,
-                Name = "Roy Kollow",
-                Team = "Red Warriors",
-                Nationality = "Danish",
-                Age = 27
-            }
-        };
+            _dataContext = context;
+        }
 
         [HttpGet]
-        public ActionResult<List<FootballPlayer>> Getplayers()
+        public async Task<ActionResult<List<FootballPlayer>>> Getplayers()
         {
-            return Ok(players);
+            return Ok(await _dataContext.players.ToListAsync());
         }
 
         [HttpGet("{id}")]
-        public ActionResult<FootballPlayer> GetPlayerById(int id)
+        public async Task<ActionResult<FootballPlayer>> GetPlayerById(int id)
         {
-            var player = players.FirstOrDefault(x => x.Id == id);
+            var player = await _dataContext.players.FindAsync(id);
             if (player == null)
                 return NotFound();
             return Ok(player);
         }
 
         [HttpPost]
-        public ActionResult AddPlayer(FootballPlayer newPlayer)
+        public async Task<ActionResult<FootballPlayer>> AddPlayer(FootballPlayer newPlayer)
         {
             if (newPlayer == null)
                 return BadRequest();
-            players.Add(newPlayer);
+            _dataContext.players.Add(newPlayer);
+            await _dataContext.SaveChangesAsync();
             return CreatedAtAction(nameof(GetPlayerById), new { id = newPlayer.Id }, newPlayer);
         }
 
         [HttpPut("{id}")]
-        public IActionResult UpdatePlayer(int id, FootballPlayer updatedPlayer)
+        public async Task<IActionResult> UpdatePlayer(int id, FootballPlayer updatedPlayer)
         {
-           var player = players.FirstOrDefault(x => x.Id == id);
-           if(player == null)
+            var player = await _dataContext.players.FindAsync(id);
+            if (player == null)
                 return NotFound();
 
-           player.Id = updatedPlayer.Id;
-           player.Name = updatedPlayer.Name;
-           player.Age = updatedPlayer.Age;
-           player.Team = updatedPlayer.Team;
-           player.Nationality = updatedPlayer.Nationality;
+            player.Id = updatedPlayer.Id;
+            player.Name = updatedPlayer.Name;
+            player.Age = updatedPlayer.Age;
+            player.Team = updatedPlayer.Team;
+            player.Nationality = updatedPlayer.Nationality;
+
+            await _dataContext.SaveChangesAsync();
 
             return Created();
 
         }
 
         [HttpDelete("{id}")]
-        public IActionResult DeletePlayer(int id)
+        public async Task<IActionResult> DeletePlayer(int id)
         {
-            var player = players.FirstOrDefault(x => x.Id == id);
+            var player = await _dataContext.players.FindAsync(id);
             if (player == null)
                 return NotFound();
 
-            players.Remove(player);
+            _dataContext.players.Remove(player);
+            await _dataContext.SaveChangesAsync();
             return Ok();
         }
     }
---
+++ FootballApi/Data/DataContext.cs
@@ -0,0 +1,73 @@
+﻿using FootballApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FootballApi.Data
+{
+    public class DataContext : DbContext
+    {
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options){}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<FootballPlayer>().HasData(
+            new FootballPlayer
+            {
+                Id = 1,
+                Name = "Dave Rogers",
+                Team = "Rushie United",
+                Nationality = "Scottish",
+                Age = 23
+            },
+            new FootballPlayer
+            {
+                Id = 2,
+                Name = "Jimmy Fred",
+                Team = "Astro Rovers",
+                Nationality = "French",
+                Age = 30
+            },
+            new FootballPlayer
+            {
+                Id = 3,
+                Name = "Roy Kollow",
+                Team = "Red Warriors",
+                Nationality = "Danish",
+                Age = 27
+            });
+
+            modelBuilder.Entity<FootballTeam>().HasData(
+            new FootballTeam
+            {
+               Id = 1,
+               Name = "Yuko warriors",
+               YearFounded = 1945,
+               Country = "France",
+               Manager = "Mr T"
+            },
+            new FootballTeam
+            {
+                Id = 2,
+                Name = "Green Lamp United",
+                YearFounded = 1890,
+                Country = "England",
+                Manager = "Mr Q"
+            },
+            new FootballTeam
+            {
+                Id = 3,
+                Name = "Red Sea United",
+                YearFounded = 1956,
+                Country = "Brazil",
+                Manager = "Mr Z"
+            });
+        }
+
+
+        public DbSet<FootballPlayer> players { get; set; }
+        public DbSet<FootballTeam> teams { get; set; }
+        
+    }
+}
