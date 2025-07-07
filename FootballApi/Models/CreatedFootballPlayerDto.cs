121c91405f3d15934716d9b23d8fdaa7ef9f24f5
Graeme Hill <49688915+Hillbill1987@users.noreply.github.com>
2025-07-07 11:43:17 +0100

    added a dto object for the create and update of objects

---
+++ FootballApi/Models/CreatedFootballPlayerDto.cs
@@ -0,0 +1,10 @@
+﻿namespace FootballApi.Models
+{
+    public class CreatedFootballPlayerDto
+    {
+        public string Name { get; set; }
+        public string Team { get; set; }
+        public int Age { get; set; }
+        public string Nationality { get; set; }
+    }
+}
